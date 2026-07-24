fb3a7c320a90a29f186e8ded0e025b8f7c886cd2
Gustavo Nicoliche <gustavodelagama@icloud.com>
2026-07-24 00:52:11 -0300

    last modifications

--- autom.py
+++ autom.py
@@ -1,4 +1,7 @@
 import pyautogui
 
 print('Resolução da tela: ', pyautogui.size())
-print('Posição atual do mouse: ', pyautogui.displayMousePosition())
+print('Posição atual do mouse: ', pyautogui.position())
+
+pixelcolor = pyautogui.pixel(100, 200)
+print('Cor do pixel (100, 200): ', pixelcolor)
