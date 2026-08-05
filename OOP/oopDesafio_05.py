9bd697748c4a9de575ec4eb69269325c848d1268
Gustavo Nicoliche <gustavodelagama@icloud.com>
2026-08-05 13:26:54 -0300

    created a game center hub, where the user can create a profile and add his favorite games. When user wants to finish favorite games list, program shows a card with players informations

---
+++ OOP/oopDesafio_05.py
@@ -0,0 +1,53 @@
+''' ENUNCIADO
+Crie a classe Gamer, onde podemos cadastrar noome, nick e jogos favoritos de uma
+pessoa. Crie também um étodo que permita mostrar a ficha desse gamer.
+'''
+from rich import print
+from rich.panel import Panel
+
+class Gamer:
+    def __init__(self, nome, nick, jogosFavoritos = 'nenhum'):
+        self.nome = nome
+        self.nick = nick
+        self.favoritos = jogosFavoritos
+
+    def adicionarJogos(self):
+        jogosFavoritos = []
+        while True:
+            jogo = input('Adicione um jogo favorito à sua lista, ou digite "sair": ')
+            if jogo.lower() == "sair":
+                break
+            jogosFavoritos.append(jogo)
+        self.favoritos = jogosFavoritos
+
+    def exibirFicha(self):
+        conteudo = f'[green]Player {self.nick}[/]\n'
+        conteudo += f'[blue]{self.favoritos}[/]\n'
+        conteudo += ' \n'
+        conteudo += ':fireworks: Muito obrigado por jogar com a gente! :fireworks:'
+        ficha = Panel(conteudo, width=80, style="yellow", title="FICHA DO JOGADOR")
+        print(ficha)
+
+
+# PROGRAMA
+conteudoInicio = ':game_die: [green]Bem-vindo ao PLAY CENTER PYGAMES!!![/] :joystick:\n'
+conteudoInicio += ' \n'
+conteudoInicio += ':computer_disk: [blue]Vamos começar criando seu perfil.\n:computer_disk: No espaço abaixo informe seu [yellow]Nome[/] e seu [yellow]Nickname[/]:[/]\n'
+inicio = Panel(conteudoInicio, width = 80, style = "blue", title="PYGAMES")
+print(inicio)
+
+nome = input('Nome: ')
+nick = input('Nickname: ')
+player1 = Gamer(nome, nick)
+
+conteudoHome = f'[green] Seja bem-vindo, [yellow]{nome}[/]![/]\n'
+conteudoHome += f'[blue] Seu nick para compartilhar com outros players é: [yellow]{nick}[/].[/]\n'
+conteudoHome += f'[blue] Gostaria de adicionar jogos favoritos à sua lista? s/n [/]'
+home = Panel(conteudoHome.center(90), width = 80, style = "blue", title="PYGAMES")
+print(home)
+
+resposta = input('Resposta s/n: ')
+if resposta == "s":
+    player1.adicionarJogos()
+
+player1.exibirFicha()
